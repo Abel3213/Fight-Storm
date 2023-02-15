34669c034e45d61ff7958b745c5fd67b7793bf8e
Abel3213 <98985844+Abel3213@users.noreply.github.com>
2023-02-15 16:25:04 -0600

    heroku

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
-const socket = io.connect("https://mysterious-ravine-22554.herokuapp.com/");
+const socket = io.connect("https://fightstorm.herokuapp.com/");
 
 canvas.width = 1024
 canvas.height = 576
